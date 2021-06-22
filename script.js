80a6dbb006d43f2351af877714f5e2a534a46828
Kpazawala Windross <57118276+Kpazawala@users.noreply.github.com>
2021-06-22 17:28:08 +0000

    hamster

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 // Author:
 
 // Global UI Variables
-//let canvasDiv;
+let canvasDiv;
+let canvas;
+let textDiv;
+let textP;
 
 // Global ML Variables
-//let mobilenet;
+let mobilenet;
+let img;
 
 function setup() {
+  canvasDiv = createDiv();
+  canvas = createCanvas(640, 480);
+  canvas.parent(canvasDiv);
+  testDiv = createDiv();
+
+  textDiv = createDiv();
+  textP = createP("Model loading, please wait..");
+  textP.parent(textDiv);
+
+  img = loadImage("images/guinea-pig.jpg", imageLoaded);
 
 }
 
@@ -15,13 +29,24 @@ function draw() {
 }
 
 function imageLoaded() {
-
+  image(img, 0, 0, width, height);
+  mobilenet = ml5.imageClassifier("MobileNet", modelReady);
+ 
 }
 
 function modelReady() {
+  mobilenet.classify(canvas, gotResults);
 
 }
 
 function gotResults(error, results) {
+  if(error) {
+    console.error(error);
+  } else {
+    //console.log(results);
+    let label = results[0].label;
+    let confidence = round(results[0].confidence, 2);
+    textP.html("Label: " + label + " - Confidence: " + confidence);
+  }
 
 }
